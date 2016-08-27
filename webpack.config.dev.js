74287ffd6faea602a4977765b7338e842f80a11f
Chirag Rupani <zw78@INLTD11006.global.tesco.org>
2016-08-27 13:50:35 +0530

    Removed preset query from webpack config.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -25,7 +25,7 @@ export default {
     ],
     module: {
         loaders: [
-            { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'], query: { presets: ['es2015']} },
+            { test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel'] },
             { test: /(\.css)$/, loaders: ['style', 'css']},
             { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
             { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
